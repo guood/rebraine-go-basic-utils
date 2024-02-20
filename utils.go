619a014ecb32c0eaaa9dd2b34d9f14d83e4d0a60
Andrei Sadovodov <kdos@bk.ru>
2024-02-20 18:35:55 +0300

    version 1

--- utils.go
+++ utils.go
@@ -1,10 +1,19 @@
 ﻿package utils
 
-func Contains(a []string, x string) bool {
-	for _, n := range a {
-    	if x == n {
-       		return true
-		}
-	}
-	return false
+func InSlice(a []string, x string) bool {
+  for _, n := range a {
+     if x == n {
+        return true
+     }
+  }
+  return false
+}
+
+func ContainsInt(a []int, x int) bool {
+  for _, n := range a {
+     if x == n {
+        return true
+     }
+  }
+  return false
 }
